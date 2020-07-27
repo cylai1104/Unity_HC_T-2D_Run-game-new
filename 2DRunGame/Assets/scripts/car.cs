677eb7c80dd14fa6937746ffd31165f332bd1f4d
cylai1104 <67904659+cylai1104@users.noreply.github.com>
2020-07-27 21:29:53 +0800

    認識方法與事件

--- 2DRunGame/Assets/scripts/car.cs
+++ 2DRunGame/Assets/scripts/car.cs
@@ -5,7 +5,7 @@ public class car : MonoBehaviour
 
     // 欄位 field:儲存資料
     // 欄位語法 
-    // 欄位類型 欄位名稱 結束符號
+    // 修飾詞 欄位類型 欄位名稱 結束符號
     // 整數 int:沒有小數點的數值，例:1、100、-999
     // 浮點數 float:有小數點的數值，例:1.234、-999.9
     // 字串 string:文字，例:紅水、kid@gmail.com、3213salkj
@@ -13,7 +13,7 @@ public class car : MonoBehaviour
 
     // 資料
     // 品牌、cc 數、重量、顏色、速度、是否有天窗
-    string brand;
+    public string brand;
     int cc;
     float weight;
     bool window;
---
+++ 2DRunGame/Assets/場景/player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    #region 欄位
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 5;
+    [Header("跳越高度"), Range(0, 1000)]
+    public int jump = 350;
+    [Header("血量"), Range(0, 2000)]
+    public bool isGround;
+    public float hp = 500;
+    public int coin;
+
+    [Header("音效區域")]
+    public AudioClip soundHit;
+    public AudioClip soundSlide;
+    public AudioClip soundJump;
+    public AudioClip soundCoin;
+
+    public Animator ani;
+    public Rigidbody2D rid;
+    public CapsuleCollider2D cap;
+    #endregion
+
+    #region 方法
+    /// <summary>
+    ///  移動
+    /// </summary>
+    private void Move()
+    {
+
+    }
+
+    /// <summary>
+    ///  跳躍
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+
+    /// <summary>
+    ///  滑行
+    /// </summary>
+    private void Slide()
+    {
+    
+    }
+
+    /// <summary>
+    ///  吃金幣
+    /// </summary>
+    private void Coin()
+    {
+
+    }
+
+    /// <summary>
+    ///  受傷
+    /// </summary>
+    private void Hit()
+    {
+
+    }
+
+    /// <summary>
+    ///  死亡
+    /// </summary>
+    private void Die()
+    {
+
+    }
+
+    /// <summary>
+    ///  過關
+    /// </summary>
+    private void Clear()
+    {
+
+    }
+    #endregion
+
+    #region 事件
+    
+    #endregion
+}
